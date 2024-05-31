232f00d90b6d9182b620d15b28289cfe9e55526a
Will Colding <william.colding@gmail.com>
2024-05-31 13:57:39 -0400

    Bump version

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ class SWRWorld(World):
     options_dataclass = SWROptions
     options: SWROptions
 
-    required_client_version = (0, 5, 2)
+    required_client_version = (0, 6, 0)
 
     item_name_to_id = get_item_name_to_id()
     location_name_to_id = get_location_name_to_id()
